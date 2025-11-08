3b39438ccf2e277e8212a4c5921bab88436d92e2
Le Van Nhu Y <31586323+duongacy@users.noreply.github.com>
2025-11-09 01:17:37 +0700

    chore: remove unused React imports

--- components/ColorModeToggle.tsx
+++ components/ColorModeToggle.tsx
@@ -2,7 +2,6 @@
 
 import { IconButton, useColorMode } from "@chakra-ui/react";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
-import React from "react";
 
 export default function ColorModeToggle() {
   const { colorMode, toggleColorMode } = useColorMode();
