dc10f8981dd43d72c7143f649147c1c7a0d697d8
Le Van Nhu Y <31586323+duongacy@users.noreply.github.com>
2025-11-14 01:10:43 +0700

    update environment

--- lib/consts.ts
+++ lib/consts.ts
@@ -2,3 +2,4 @@ export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
 export const API_PORT = process.env.NEXT_PUBLIC_API_PORT || "";
 export const API_BASE_URL = `${API_URL}:${API_PORT}`;
 export const APP_PORT = process.env.NEXT_PUBLIC_APP_PORT || "";
+export const HOST_NAME = process.env.NEXT_PUBLIC_HOST_NAME || "";
--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['127.0.0.1'],
+  allowedDevOrigins: ["127.0.0.1"],
   images: {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "127.0.0.1",
-        port: "1337",
+        hostname: process.env.NEXT_PUBLIC_HOST_NAME,
+        port: process.env.NEXT_PUBLIC_API_PORT,
       },
     ],
   },
